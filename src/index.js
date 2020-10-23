44f2f5adf1b175e411e2fa43e8cebe09df4406c4
Hussein <mamunanaqvi96@gmail.com>
2020-10-23 22:04:43 +0500

    prepares folder structure for redux advanced

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import App from './App';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
-import reducer from './store/reducer';
+import reducer from './store/reducers/burgerBuilder';
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store= {store}>
---
+++ src/store/actions/burgerBuilder.js
@@ -0,0 +1,1 @@
+//only sync action creators
