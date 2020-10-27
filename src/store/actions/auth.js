4fb5132ce6492c7c727deabe0f24cbf80d446ab8
Hussein <mamunanaqvi96@gmail.com>
2020-10-27 21:15:26 +0500

    removes key

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -32,7 +32,7 @@ export const auth = (email, password) => {
     };
     axios
       .post(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCirbqpFlMWxsH7FERIl9xiEpv5O7XbJ88",
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]",
         authData
       )
       .then((response) => {
