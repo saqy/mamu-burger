47e8707102e491831b46cf1b8ddde3fb32901987
Hussein <mamunanaqvi96@gmail.com>
2020-10-31 00:57:48 +0500

    handles sign-in with Saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,11 +1,11 @@
 //allow us to listen to certain actions and do something when they occur
 import { takeEvery } from "redux-saga/effects";
 import * as actionTypes from "../actions/actionTypes";
-import { logoutSaga, checkAuthTimeoutSaga, authUserSaga } from "./auth";
+import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from "./auth";
 
 export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
   yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-
+  yield takeEvery(actionTypes.AUTH_CHECK_INITIAL_STATE, authCheckStateSaga);
 }
