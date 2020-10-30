edc0a6b7ca8bd5617264497c6e95ee63fc42149d
Hussein <mamunanaqvi96@gmail.com>
2020-10-31 01:33:09 +0500

    handles burger builder side effects with Saga

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,6 +2,8 @@ export {
   addIngredients,
   removeIngredients,
   initIngredients,
+  setIngredients,
+  fetchIngredientsFailed,
 } from "./burgerBuilder";
 export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
 export { auth, logout, setAuthRedirectPath, authCheckState, logoutSucceed, authStart, authSuccess, authFail, checkAuthTimeout } from "./auth";
